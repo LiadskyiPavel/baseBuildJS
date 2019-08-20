413ca5afef1ca9f92822f1eddbc08c8cfea36490
LiadskyiPavel <pavelliadskyi@gmail.com>
2019-08-20 20:20:28 +0300

    first js

--- dist/js/index.js
+++ dist/js/index.js
@@ -93,7 +93,35 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-$(document).redy(function () {});
+// import $ from 'jquery';
+$(document).ready(function () {
+  $('.navbar input').addClass('searchEngine');
+  $('.searchEngine').hide();
+  $('.navbar button').addClass('searchButton');
+  $('.searchButton').on('click', function () {
+    $('.searchEngine').show(500).toggleClass('open');
+  });
+  var search = $('.searchEngine');
+  var btn = $('searchButton');
+  $(document).mouseup(function (e) {
+    if (!search.is(e.target)) {
+      search.hide(500).removeClass('open');
+    }
+
+    ;
+  });
+  search.focus(function () {
+    if ($(this).val() === 'Search') {
+      console.log(this.val());
+    } else {
+      btn.off();
+    }
+  }); // if ($('.open'))  {
+  // 	$('.serchButton').off('click');
+  // btn.submit()
+  // }
+  // .submit()
+});
 
 /***/ })
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,40 @@
-$(document).redy(function() {
+// import $ from 'jquery';
+$(document).ready(function() {
+
+	$('.navbar input').addClass('searchEngine');
+
+	$('.searchEngine').hide();
+
+	$('.navbar button').addClass('searchButton');
+
+	$('.searchButton').on('click', () => {
+		$('.searchEngine').show(500).toggleClass('open')
+	});
+
+	let search = $('.searchEngine');
+	let btn = $('searchButton');
+
+	$(document).mouseup( function(e) {
+
+		if (!search.is(e.target)) {
+				search.hide(500).removeClass('open')
+		};
+			
+	});
+
+	search.focus( function() {
+		if ($(this).val() === 'Search') {
+			console.log(this.val())
+		} else {
+			btn.off()
+		}
+	});
+		// if ($('.open'))  {
+		// 	$('.serchButton').off('click');
+			// btn.submit()
+		// }
+			// .submit()
+
 
 
 
